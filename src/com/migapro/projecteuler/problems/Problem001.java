30d38a663c203880e73136363c115eb0f1f3e0d7
Miguel Kano <miga88k@gmail.com>
2014-04-04 11:55:46 -0500

    problem 001 solved

--- src/com/migapro/projecteuler/problems/Problem001.java
+++ src/com/migapro/projecteuler/problems/Problem001.java
@@ -4,9 +4,13 @@ package com.migapro.projecteuler.problems;
 public class Problem001 extends Problem {
 
 	@Override
-	public String solve() {
-
-		return "";
+	public int solve() {
+		int sum = 0;
+		for (int i = 0; i < 1000; i++)
+			if (i % 3 == 0 || i % 5 == 0)
+				sum += i;
+		
+		return sum;
 	}
 
 }
